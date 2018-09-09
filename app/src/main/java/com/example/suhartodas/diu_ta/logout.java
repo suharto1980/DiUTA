07b35d3289459b0914fe072fd4f5a340851edf3e
suharto1980 <suvro.suharto@gmail.com>
2018-09-10 00:16:55 +0600

    Commit

---
+++ app/src/main/java/com/example/suhartodas/diu_ta/logout.java
@@ -0,0 +1,13 @@
+package com.example.suhartodas.diu_ta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class logout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+    }
+}
